9ba16dea5562e625d7518dd5ec61f73c208efcb8
NataShasasa <151643541+NataShasasa@users.noreply.github.com>
2024-05-16 19:04:22 +0200

    perdezh

---
+++ perdezh.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<title>Пример воспроизведения звука</title>
+</head>
+<body>
+
+<button id="playSound">Воспроизвести звук</button>
+
+<audio id="soundClip" src="path_to_your_audio_file.mp3"></audio>
+
+<script>
+document.getElementById('playSound').addEventListener('click', function() {
+    var audio = document.getElementById('soundClip');
+    audio.play();
+});
+</script>
+
+</body>
+</html>
